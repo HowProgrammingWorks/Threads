60513849aa3a7a3edd65b21eabad7b224d4d80bb
tshemsedinov <timur.shemsedinov@gmail.com>
2019-02-01 16:58:30 +0200

    Start http server from worker

---
+++ JavaScript/3-http.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const threads = require('worker_threads');
+const http = require('http');
+
+const port = 8000;
+
+threads.parentPort.postMessage({ name: 'started', port });
+
+const routing = {
+  '/': async (req, res) => {
+    return { status: res.statusCode };
+  },
+  '/api/method': async (req, res) => {
+    return { status: res.statusCode };
+  },
+};
+
+const types = {
+  object: JSON.stringify,
+  string: s => s,
+  number: n => n.toString(),
+  undefined: () => 'not found',
+};
+
+http.createServer(async (req, res) => {
+  const handler = routing[req.url];
+  if (!handler) {
+    res.end('Handler not found');
+    return;
+  }
+  const data = await handler(req, res);
+  const type = typeof data;
+  const serializer = types[type];
+  const result = serializer(data);
+  res.end(result);
+}).listen(port);
---
+++ JavaScript/3-server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const threads = require('worker_threads');
+const { Worker } = threads;
+
+const worker = new Worker('./3-http.js');
+
+worker.on('message', msg => {
+  if (msg.name === 'started') {
+    console.log(`HTTP Server Started on ${msg.port}`);
+  }
+});
+
+process.on('SIGTERM', () => {
+  worker.terminate(() => {
+    console.log('HTTP Server Stopped');
+  });
+});
